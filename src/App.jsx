0681f631c86a8a5849bbc6fa47579b3258ff31db
Bohyun Kang <bbohyun@kakao.com>
2022-11-28 14:56:35 +0900

    페이지 라우팅 처리
    - react-router-dom으로 프로젝트에 필요한 페이지 라우팅

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,25 @@
+import { Routes, Route } from 'react-router-dom';
+
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage';
+import ProductsPage from './pages/ProductsPage';
+import ProductDetailPage from './pages/ProductDetailPage';
+import OrdersPage from './pages/OrdersPage';
+import OrderDetailPage from './pages/OrderDetailPage';
+import OrderPage from './pages/OrderPage';
+
 export default function App() {
   return (
-    <div>App</div>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/signup" element={<SignupPage />} />
+      <Route path="/products" element={<ProductsPage />} />
+      <Route path="/products/:id" element={<ProductDetailPage />} />
+      <Route path="/orders" element={<OrdersPage />} />
+      <Route path="/orders/:id" element={<OrderDetailPage />} />
+      <Route path="/order" element={<OrderPage />} />
+    </Routes>
   );
 }
