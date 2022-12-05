fb15c9297487c480c1586b42d5bb1d3d36bbb567
Bohyun Kang <bbohyun@kakao.com>
2022-12-06 00:19:48 +0900

    비로그인 시 접근 못하는 라우팅 처리
    - 주문 조회, 주문하기, 주문 상세 페이지

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
+import { useEffect } from 'react';
+
 import { Routes, Route } from 'react-router-dom';
 
 import styled, { ThemeProvider } from 'styled-components';
 
 import { Reset } from 'styled-reset';
 
+import { useLocalStorage } from 'usehooks-ts';
+
+import { apiService } from './services/ApiService';
+
 import GlobalStyle from './styles/GlobalStyle';
 
 import theme from './styles/theme';
+
+import useUserStore from './hooks/useUserStore';
+
+import ProtectedRoute from './ProtectedRoute';
+
+import Header from './components/Header';
 
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
@@ -16,9 +28,20 @@ import ProductDetailPage from './pages/ProductDetailPage';
 import OrdersPage from './pages/OrdersPage';
 import OrderDetailPage from './pages/OrderDetailPage';
 import OrderPage from './pages/OrderPage';
-import Header from './components/Header';
 
 export default function App() {
+  const [accessToken] = useLocalStorage('accessToken', '');
+
+  const userStore = useUserStore();
+
+  useEffect(() => {
+    apiService.setAccessToken(accessToken);
+
+    if (accessToken) {
+      userStore.fetchUser();
+    }
+  }, [accessToken]);
+
   return (
     <ThemeProvider theme={theme}>
       <Reset />
@@ -31,9 +54,31 @@ export default function App() {
           <Route path="/signup" element={<SignupPage />} />
           <Route path="/products" element={<ProductsPage />} />
           <Route path="/products/:id" element={<ProductDetailPage />} />
-          <Route path="/orders" element={<OrdersPage />} />
-          <Route path="/orders/:id" element={<OrderDetailPage />} />
-          <Route path="/order" element={<OrderPage />} />
+          <Route
+            path="/orders"
+            element={(
+              <ProtectedRoute accessToken={accessToken}>
+                <OrdersPage />
+              </ProtectedRoute>
+            )}
+          />
+          <Route
+            path="/orders/:id"
+            element={(
+              <ProtectedRoute accessToken={accessToken}>
+                <OrderDetailPage />
+              </ProtectedRoute>
+            )}
+          />
+          <Route
+            path="/order"
+            element={(
+              <ProtectedRoute accessToken={accessToken}>
+                <OrderPage />
+              </ProtectedRoute>
+            )}
+          />
+          <Route path="*" element={<HomePage />} />
         </Routes>
       </Main>
     </ThemeProvider>
