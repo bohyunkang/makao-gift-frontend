30acd0d8ebc3e513aee5e2f03ae639bf674f45f7
Bohyun Kang <bbohyun@kakao.com>
2022-12-08 16:53:35 +0900

    새로고침 시 데이터 날라감 방지
    - 새로고침할 때마다 setAccessToken

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,14 @@ import ReactDOM from 'react-dom/client';
 
 import { BrowserRouter } from 'react-router-dom';
 
+import { apiService } from './services/ApiService';
+
 import App from './App';
+
+const data = localStorage.getItem('accessToken');
+const accessToken = JSON.parse(data);
+
+apiService.setAccessToken(accessToken);
 
 const container = document.getElementById('app');
 const root = ReactDOM.createRoot(container);
